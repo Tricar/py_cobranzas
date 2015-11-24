0331b0e9b51ecf71919ff7fdb463485ad63e0b9f
Tricar <dajohlopez@gmail.com>
2015-11-24 15:16:55 -0500

    Merge origin/master
    Conflicts:
    	cobranzaTricar/nbproject/faces-config.NavData
    	cobranzaTricar/src/java/Bean/vehiculoBean.java
    	cobranzaTricar/web/views/template.xhtml

--- cobranzaTricar/src/java/utiles/precio.java
+++ cobranzaTricar/src/java/utiles/precio.java
@@ -15,16 +15,16 @@ public class precio {
 
             switch (model) {
                 case "VX150":
-                    precio = BigDecimal.valueOf(5400.00);
+                    precio = BigDecimal.valueOf(5450.00);
                     break;
                 case "MTXC VX150":
-                    precio = BigDecimal.valueOf(6600.00);
+                    precio = BigDecimal.valueOf(6800.00);
                     break;
                 case "TX200-C":
-                    precio = BigDecimal.valueOf(6700.00);
+                    precio = BigDecimal.valueOf(7100.00);
                     break;
                 case "TX250ZH-E":
-                    precio = BigDecimal.valueOf(7700.00);
+                    precio = BigDecimal.valueOf(7600.00);
                     break;
                 case "TX250ZH-M":
                     precio = BigDecimal.valueOf(8400.00);
@@ -33,40 +33,40 @@ public class precio {
                     precio = BigDecimal.valueOf(2750.00);
                     break;
                 case "TX110-A":
-                    precio = BigDecimal.valueOf(2450.00);
+                    precio = BigDecimal.valueOf(2750.00);
                     break;
                 case "VX110-7":
-                    precio = BigDecimal.valueOf(2900.00);
+                    precio = BigDecimal.valueOf(3000.00);
                     break;
                 case "VX110-9":
-                    precio = BigDecimal.valueOf(2850.00);
+                    precio = BigDecimal.valueOf(3000.00);
                     break;
                 case "VX125-2T":
                     precio = BigDecimal.valueOf(3100.00);
                     break;
                 case "TX150-C":
-                    precio = BigDecimal.valueOf(2800.00);
+                    precio = BigDecimal.valueOf(2990.00);
                     break;
                 case "TX150-R":
-                    precio = BigDecimal.valueOf(3950.00);
+                    precio = BigDecimal.valueOf(4400.00);
                     break;
                 case "VX150-8":
-                    precio = BigDecimal.valueOf(3000.00);
+                    precio = BigDecimal.valueOf(3450.00);
                     break;
                 case "VX150-25":
                     precio = BigDecimal.valueOf(4400.00);
                     break;
                 case "VX150GY-11B":
-                    precio = BigDecimal.valueOf(3800.00);
+                    precio = BigDecimal.valueOf(4050.00);
                     break;
                 case "VX150GY-11A":
                     precio = BigDecimal.valueOf(4600.00);
                     break;
                 case "VX200GY-V2":
-                    precio = BigDecimal.valueOf(4200.00);
+                    precio = BigDecimal.valueOf(3950.00);
                     break;
                 case "TX150":
-                    precio = BigDecimal.valueOf(4600.00);
+                    precio = BigDecimal.valueOf(4500.00);
                     break;
 
             }
