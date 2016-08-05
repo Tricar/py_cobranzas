8c01c81c9a8c31a254b5bfb34e9d0f214af14f22
juliocell <cambiunfox_syn@hotmail.com>
2016-08-04 19:54:00 -0500

    Merge origin/master
    Conflicts:
    	cobranzaTricar/web/despacho/formdespacho.xhtml

--- cobranzaTricar/src/java/utiles/dbManager.java
+++ cobranzaTricar/src/java/utiles/dbManager.java
@@ -11,8 +11,8 @@ public class dbManager {
     public static Connection getConnection() {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            Connection con = DriverManager.getConnection("jdbc:sqlserver://ricardo-pc:1433;databaseName=cobranzas",
-                    "sa", "admin");
+            Connection con = DriverManager.getConnection("jdbc:sqlserver://server-pc:1433;databaseName=cobranzas",
+                    "sa", "tricar2012");
             return con;
         } catch (Exception ex) {
             System.out.println("Database.getConnection() Error -->" + ex.getMessage());
