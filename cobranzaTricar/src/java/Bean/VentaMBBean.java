bcf95282690f860f43460f65c38b83a0ed279360
juliocell <cambiunfox_syn@hotmail.com>
2016-09-08 15:35:13 -0500

    Merge origin/master
    Conflicts:
    	cobranzaTricar/src/java/Bean/PerfilBean.java
    	cobranzaTricar/src/java/Bean/ventaBean.java
    	cobranzaTricar/src/java/Model/Perfil.hbm.xml
    	cobranzaTricar/src/java/Model/Perfil.java
    	cobranzaTricar/src/java/hibernate.cfg.xml
    	cobranzaTricar/web/sistema/perfil.xhtml
    	cobranzaTricar/web/views/index.xhtml
    	cobranzaTricar/web/views/template.xhtml

--- cobranzaTricar/src/java/Bean/VentaMBBean.java
+++ cobranzaTricar/src/java/Bean/VentaMBBean.java
@@ -27,42 +27,42 @@ public class VentaMBBean implements Serializable {
     public VentaMBBean() {
     }
 
-    public String generar_codigo_Articulo(Tipoarticulo objtipoarticulo) {
-        int vcorre = 1;
-        String sql = "";
-        vcodigoart = "";
-
-        try {
-            dbManager dbm = new dbManager();
-            Connection con = dbm.getConnection();
-            if (con == null) {
-                throw new NullPointerException(dbm.geterror());
-            }
-            sql = "SELECT correlativo FROM articulo where codtipart='" + objtipoarticulo.getCodtipart() + "' ORDER BY correlativo";
-            PreparedStatement st = con.prepareStatement(sql, 1005, 1007);
-            ResultSet rs = st.executeQuery();
-            rs.afterLast();
-            if (rs.previous()) {
-                vcorre = Integer.parseInt(rs.getString("correlativo"));
-                vcorre++;
-            }
-            for (int i = 1; i < 4 - String.valueOf(vcorre).length(); i++) {
-                vcerosart = vcerosart + "0";
-            }
-
-            vcodigoart = vcerosart + vcorre;
-            rs.close();
-            st.close();
-            con.close();
-            System.out.println(vcerosart + vcorre);
-
-            vcodigofinalarticulo = objtipoarticulo.getAbreviatura() + vcodigoart;
-
-        } catch (Exception e) {
-            e.getMessage();
-            System.out.println(e.getMessage());
-        }
-        return (vcodigofinalarticulo);
-    }
+//    public String generar_codigo_Articulo(Tipoarticulo objtipoarticulo) {
+//        int vcorre = 1;
+//        String sql = "";
+//        vcodigoart = "";
+//
+//        try {
+//            dbManager dbm = new dbManager();
+//            Connection con = dbm.getConnection();
+//            if (con == null) {
+//                throw new NullPointerException(dbm.geterror());
+//            }
+//            sql = "SELECT correlativo FROM articulo where codtipart='" + objtipoarticulo.getCodtipart() + "' ORDER BY correlativo";
+//            PreparedStatement st = con.prepareStatement(sql, 1005, 1007);
+//            ResultSet rs = st.executeQuery();
+//            rs.afterLast();
+//            if (rs.previous()) {
+//                vcorre = Integer.parseInt(rs.getString("correlativo"));
+//                vcorre++;
+//            }
+//            for (int i = 1; i < 4 - String.valueOf(vcorre).length(); i++) {
+//                vcerosart = vcerosart + "0";
+//            }
+//
+//            vcodigoart = vcerosart + vcorre;
+//            rs.close();
+//            st.close();
+//            con.close();
+//            System.out.println(vcerosart + vcorre);
+//
+//            vcodigofinalarticulo = objtipoarticulo.getAbreviatura() + vcodigoart;
+//
+//        } catch (Exception e) {
+//            e.getMessage();
+//            System.out.println(e.getMessage());
+//        }
+//        return (vcodigofinalarticulo);
+//    }
 
 }
